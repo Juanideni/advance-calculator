3af58bbb7fcbd608e703506a70bef2010f62e675
Juan Denipoti <juanidenipoti11@gmail.com>
2021-07-12 17:51:46 -0300

    Display component done

---
+++ src/Display.js
@@ -0,0 +1,17 @@
+
+    function Display(){
+        return(
+            <>
+            <ul>
+                <li><h3>- Suma: <span className="suma">0</span></h3></li>
+                <li><h3>- Resta: <span className="resta">0</span></h3></li>
+                <li><h3>- Multiplicacion: <span className="multiplicacion">0</span></h3></li>
+                <li><h3>- Division: <span className="division">0</span></h3></li>
+                <li><h3>- Potencia: <span className="potencia">0</span></h3></li>
+                <li><h3>- Raiz cuadrada: <span className="raiz cuadrada">0</span></h3></li>
+                <li><h3>- Hipotenusa: <span className="hipotenusa">0</span></h3></li>
+            </ul>
+            </>
+        )
+    }
+    export default Display;
