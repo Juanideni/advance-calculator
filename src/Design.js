6656c15ee50fe57a6b747646f147a748f9390bb5
Juan Denipoti <juanidenipoti11@gmail.com>
2021-07-14 18:07:01 -0300

    React done

--- src/Design.js
+++ src/Design.js
@@ -34,13 +34,13 @@ function Design(){
         </div>
         <div>
             <ul>
-                <li><h3>- Suma: <span id="suma">{firstNumber+"+"+secondNumber+"= "}</span></h3></li>
-                <li><h3>- Resta: <span id="resta">{firstNumber+"-"+secondNumber+"= "}</span></h3></li>
-                <li><h3>- Multiplicacion: <span id="multiplicacion">{firstNumber+"*"+secondNumber+"= "}</span></h3></li>
-                <li><h3>- Division: <span id="division"></span>{firstNumber+"/"+secondNumber+"= "}</h3></li>
-                <li><h3>- Potencia: <span id="potencia"></span>{firstNumber+"^"+secondNumber+"= "}</h3></li>
-                <li><h3>- Raiz cuadrada: <span id="raiz cuadrada">{firstNumber+" √"+secondNumber+"= "}</span></h3></li>
-                <li><h3>- Hipotenusa: <span id="hipotenusa">{"("+firstNumber+")^2 + ("+secondNumber+")^2= "}</span></h3></li>
+                <li><h3>- Suma: <span id="suma">{firstNumber+"+"+secondNumber+"= "+Number((firstNumber*1)+(secondNumber *1))}</span></h3></li>
+                <li><h3>- Resta: <span id="resta">{firstNumber+"-"+secondNumber+"= "+Number(firstNumber-secondNumber)}</span></h3></li>
+                <li><h3>- Multiplicacion: <span id="multiplicacion">{firstNumber+"*"+secondNumber+"= "+firstNumber*secondNumber}</span></h3></li>
+                <li><h3>- Division: <span id="division"></span>{firstNumber+"/"+secondNumber+"= "+firstNumber/secondNumber}</h3></li>
+                <li><h3>- Potencia: <span id="potencia"></span>{firstNumber+"^"+secondNumber+"= "+Math.pow(firstNumber, secondNumber)}</h3></li>
+                <li><h3>- Raiz cuadrada: <span id="raiz cuadrada">{firstNumber+" √"+secondNumber+"= "+Math.pow(firstNumber, (1/secondNumber))}</span></h3></li>
+                <li><h3>- Hipotenusa: <span id="hipotenusa">{"("+firstNumber+")^2 + ("+secondNumber+")^2= "+Number((firstNumber*firstNumber)+(secondNumber*secondNumber))}</span></h3></li>
             </ul>
         </div>
         </>
